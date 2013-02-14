0160226e86f52c5f2707569af04b92493bf8df59
Shoghi Cervantes Pueyo <shoghicp@gmail.com>
2013-02-14 20:40:19 +0100

    Updated tests path

--- BinaryRWTest.php
+++ BinaryRWTest.php
@@ -1,6 +1,6 @@
 <?php
 
-require_once(dirname(__FILE__)."/src/dependencies.php");
+require_once(dirname(__FILE__)."/../src/dependencies.php");
 
 class BinaryRWTest extends PHPUnit_Framework_TestCase{
 	public function testRead(){
